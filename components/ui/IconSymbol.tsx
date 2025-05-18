3dc06e6de35c4e8e0f87ba50a25b60e0a9256d7c
Adina Stoica <adina.stoica@gmail.com>
2025-05-17 20:01:42 -0400

    Add icon

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -21,6 +21,8 @@ const MAPPING = {
     "paperplane.fill": "send",
     "chevron.left.forwardslash.chevron.right": "code",
     "chevron.right": "chevron-right",
+    "photo.on.rectangle": "image",
+    pencil: "create",
 } as IconMapping;
 
 /**
